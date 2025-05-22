8428636387326bc410078c59e10319bd8963bd83
lymanyang <lymanyang@tencent.com>
2025-05-22 17:00:18 +0800

    add fzf find register cmd

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -3,5 +3,6 @@ return {
   keys = {
     { "<leader>,", "<cmd>FzfLua files<cr>", desc = "find files" },
     { "<leader><leader>", "<cmd>FzfLua buffers<cr>", desc = "find buffers" },
+    { "<leader>v", "<cmd>FzfLua registers<cr>", desc = "find registers" },
   },
 }
