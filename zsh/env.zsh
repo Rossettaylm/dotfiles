6800853a451191c72032be5dec6e4e9fdbb69a91
lymanyang <lymanyang@tencent.com>
2026-01-06 09:00:01 +0800

    日常同步.config - 2026-01-06 09:00:01

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -39,6 +39,15 @@ export PATH=$PATH:$HOME/.cargo/bin
 # yazi
 export PATH=$PATH:$HOME/Softwares/yazi-aarch64-apple-darwin/
 
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# ohpm (HarmonyOS Package Manager)
+export PATH="$PATH:/Applications/DevEco-Studio.app/Contents/tools/ohpm/bin"
+
+
 
 
 #zlib ark
